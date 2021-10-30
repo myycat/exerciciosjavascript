0c4327aff091db4f9b0a97e73dacc022ee4a732c
myycat <welvesferreira@gmail.com>
2021-10-30 10:54:38 -0300

    inserindo valores do console

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ var idade2 = 23;
 alert("Seja Bem Vindo" + nome);
 alert(nome + " tem " + idade + " anos de idade");
 alert(idade+idade2);
+
+//Visualização do resultado via console
+console.log(idade2);
+console.log(idade+idade2);
